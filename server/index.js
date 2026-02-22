6e2f210342acf5b032bcd4103748bcff91cf7e8c
JasonGM01 <jakisakilla@gmail.com>
2026-02-21 20:33:36 -0800

    added front and back ends

---
+++ server/index.js
@@ -0,0 +1,13 @@
+//server
+
+const express = require('express');
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+app.get("/api", (req, res) => {
+    res.json({message:"Hello from server"});
+})
+
+app.listen(PORT, () => {
+    console.log(`server is listening on port ${PORT}`);
+});
